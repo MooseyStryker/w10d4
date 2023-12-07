c04aa715acc2c70e41cdd67c9e45f24aef8b1b42
Hao Lam <tuonghao2001@gmail.com>
2023-12-07 14:44:19 -0800

    phase4-6

--- sql-practice/phase-5/query-5.sql
+++ sql-practice/phase-5/query-5.sql
@@ -1,2 +1,15 @@
 -- Find names of the cats whose owners are both George Beatty and Melynda Abshire, or just George Beatty, or just Melynda Abshire
--- Your code here 
+-- Your code here
+
+SELECT DISTINCT
+  name
+FROM
+  cat_owners
+JOIN
+  cats ON (cat_owners.cat_id = cats.id)
+JOIN
+  owners ON (cat_owners.owner_id = owners.id)
+WHERE (
+  (first_name = "George" AND last_name = "Beatty") OR
+  (first_name = "Melynda" AND last_name = "Abshire")
+);
