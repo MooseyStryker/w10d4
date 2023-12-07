7c21e70317279dae0d4057bad2dac9038dae4183
Stryker Huber <stryker.important@gmail.com>
2023-12-07 15:23:02 -0700

    second push

--- sql-practice/phase-3/query-2.sql
+++ sql-practice/phase-3/query-2.sql
@@ -1,2 +1,12 @@
 -- Find All the Toys for Hermione's cats
--- Your code here 
+-- Your code here
+
+SELECT toys.name
+FROM toys
+WHERE cat_id = ((
+    SELECT cat_owners.cat_id
+    FROM cat_owners
+    JOIN cats ON (cat_owners.cat_id = cats.id)
+    JOIN owners ON (owners.id = cat_owners.owner_id)
+    WHERE first_name = 'Hermione'
+));
